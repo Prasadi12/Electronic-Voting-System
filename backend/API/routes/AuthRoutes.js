5ffe309cdea724e6ebf4bb8ccf2b272b735e50c3
A.N.P Jayamanna <86679140+Prasadi12@users.noreply.github.com>
2023-06-26 11:03:44 +0000

    updated structure

--- backend/API/routes/AuthRoutes.js
+++ backend/API/routes/AuthRoutes.js
@@ -19,7 +19,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   const ssn = req.body.NIC;
   const password = req.body.password;
-  const user = await Admin.findOne({ssn,password});
+  const user = await Admin.findOne({ ssn, password });
   //console.log(user) ;                     
   if (user) {
     if (user.usertype === "admin") {
@@ -41,23 +41,24 @@ router.get("/register", async (req, res) => {
     res.status(200).json(users);
     res.send("hello world");
   } catch (error) {
-    res.status(500).json({ 
-      message: error.message 
+    res.status(500).json({
+      message: error.message
     });
   }
 });
 
 router.post("/adminregister", async (req, res) => {
-  try{
+  try {
     const result = await Admin.create(req.body);
     await result.save();
 
     res.status(201).json({ result: result });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ 
-      error: error.message, 
-      er: "error happens" });
+    res.status(500).json({
+      error: error.message,
+      er: "error happens"
+    });
   }
 });
 

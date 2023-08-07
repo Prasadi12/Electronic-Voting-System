8c3744053bec4eed3a6888876a4ed179b15826e4
A.N.P Jayamanna <86679140+Prasadi12@users.noreply.github.com>
2023-08-07 04:52:08 +0000

    updat RegistrationForm.jsx

--- src/pages/registration/RegistrationForm.jsx
+++ src/pages/registration/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './RegistrationForm.css'
 import Logo from "./logo.png"
 import axios from 'axios'
@@ -6,12 +6,12 @@ import { useNavigate } from 'react-router-dom';
 
 function RegistrationForm(event) {
 
-    const navigation=useNavigate()
-    
-    
+    const navigation = useNavigate()
+
+
     const [formData, setFormData] = useState(event.target)
 
-    const registeruser = async(event) => {
+    const registeruser = async (event) => {
         console.log("run reg func")
         event.preventDefault();
 
@@ -23,86 +23,86 @@ function RegistrationForm(event) {
                 'Content-Type': 'application/json'
             }
         }).then((res) => {
-            
-            if (!alert("You have successfully registered!"))navigation("/");
+
+            if (!alert("You have successfully registered!")) navigation("/");
         }).catch((err) => {
             console.log(err)
             alert(err.message)
         })
 
         console.log(res.status);
-        if (res.status ===201) console.log(res.data);
-            if(res.status === 500){
-                console.log("fire");
-                const {error} = res.data
-                console.log(error);
-            }
+        if (res.status === 201) console.log(res.data);
+        if (res.status === 500) {
+            console.log("fire");
+            const { error } = res.data
+            console.log(error);
+        }
     };
 
     return (
         <div>
-        <div className='container div1'>
-          <div className="lo"> 
-          <img src={Logo} className = "logo1" alt=""/>
-          </div>
+            <div className='container div1'>
+                <div className="lo">
+                    <img src={Logo} className="logo1" alt="" />
+                </div>
 
-          {/*---------------headings start------------- */}
-          <div className="name">
-           <h1 className="txt1 fn">Election Name</h1>
-           <p className="txt2 fn">Year</p>
-          </div>
-           {/*---------------headings end------------- */}
-        </div>
-        <form className="form" onSubmit={registeruser}>
-
-            <h1 className=''>Registration Form</h1>
-            <div className="form-body">
-
-            <div className='fullname'>
-                <label className='form__label'>Full Name: </label>
-                <input 
-                    className="form__input" 
-                    type="text" 
-                    name="fullName"
-                    onChange={(e) =>
-                        setFormData({ 
-                            ...formData, 
-                            name: e.target.value 
-                        })
-                    }
-                placeholder="Full Name"/>
+                {/*---------------headings start------------- */}
+                <div className="name">
+                    <h1 className="txt1 fn">Election Name</h1>
+                    <p className="txt2 fn">Year</p>
+                </div>
+                {/*---------------headings end------------- */}
             </div>
+            <form className="form" onSubmit={registeruser}>
 
-            <div className='iname'>            
-            <label className="form__label" for="iName">Name with Initials :</label>
-            <input className="form__input" 
-            type="text" 
-            name="iName"
-            onChange={(e) =>
-                setFormData({
-                    ...formData,
-                    iName: e.target.value
-                })
-            }
-            placeholder="Name with Initials"/>
-        </div>
+                <h1 className=''>Registration Form</h1>
+                <div className="form-body">
 
-            <div className='nicNumber'>
-            <label className='form__label'>NIC Number: </label>
-            <input 
-            className='form__input' 
-            type="text" 
-            name="ssn" 
-            onChange={(e) =>
-                setFormData({
-                    ...formData,
-                    nicNum: e.target.value,
-                })
-            }
-            placeholder="NIC Number"/>
-        </div>
+                    <div className='fullname'>
+                        <label className='form__label'>Full Name: </label>
+                        <input
+                            className="form__input"
+                            type="text"
+                            name="fullName"
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    name: e.target.value
+                                })
+                            }
+                            placeholder="Full Name" />
+                    </div>
 
-        {/* <div className='birthday'>            
+                    <div className='iname'>
+                        <label className="form__label" for="iName">Name with Initials :</label>
+                        <input className="form__input"
+                            type="text"
+                            name="iName"
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    iName: e.target.value
+                                })
+                            }
+                            placeholder="Name with Initials" />
+                    </div>
+
+                    <div className='nicNumber'>
+                        <label className='form__label'>NIC Number: </label>
+                        <input
+                            className='form__input'
+                            type="text"
+                            name="ssn"
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    nicNum: e.target.value,
+                                })
+                            }
+                            placeholder="NIC Number" />
+                    </div>
+
+                    {/* <div className='birthday'>            
             <label className='form__label ' for="birthday">Birthday :</label>
             <input 
             className='calender form__input' 
@@ -116,7 +116,7 @@ function RegistrationForm(event) {
             }/>
         </div> */}
 
-        {/* <div className='age'>            
+                    {/* <div className='age'>            
             <label className="form__label">Age :</label>
             <input 
             className="form__input" 
@@ -131,66 +131,66 @@ function RegistrationForm(event) {
             placeholder="Age"/>
         </div> */}
 
-        <div className='address'>
-            <label className='form__label'>Address: </label>
-            <input className="form__input" type="text" 
-            name = "address"
-            onChange={(e) =>
-                setFormData({
-                    ...formData,
-                    address: e.target.value,
-                })
-            }
-            placeholder="Address"/>
-        </div>
+                    <div className='address'>
+                        <label className='form__label'>Address: </label>
+                        <input className="form__input" type="text"
+                            name="address"
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    address: e.target.value,
+                                })
+                            }
+                            placeholder="Address" />
+                    </div>
 
-        <div className='nationality'>
-            <label className='form__label'>Nationality: </label>
-            <input className="form__input" type="text" 
-            name = "nationality"
-            onChange={(e) =>
-                setFormData({
-                    ...formData,
-                    nationality: e.target.value,
-                })
-            }
-            placeholder="Nationality"/>
-        </div>                
+                    <div className='nationality'>
+                        <label className='form__label'>Nationality: </label>
+                        <input className="form__input" type="text"
+                            name="nationality"
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    nationality: e.target.value,
+                                })
+                            }
+                            placeholder="Nationality" />
+                    </div>
 
-        <div className='email'>
-            <label className='form__label'>Email: </label>
-            <input className='form__input' 
-            type="email" 
-            name = "email"
-            onChange={(e) =>
-                setFormData({
-                    ...formData,
-                    email: e.target.value,
-                })
-            }
-            placeholder="Email"/>
-        </div>
+                    <div className='email'>
+                        <label className='form__label'>Email: </label>
+                        <input className='form__input'
+                            type="email"
+                            name="email"
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    email: e.target.value,
+                                })
+                            }
+                            placeholder="Email" />
+                    </div>
 
-        <div className='contactNumber'>
-            <label className='form__label'>Contact Number: </label>
-            <input className="form__input" 
-            type="number" 
-            name = "contactNumber" 
-            onChange={(e) =>
-                setFormData({
-                    ...formData,
-                    contactNum: e.target.value,
-                })
-            }
-            placeholder="Contact Number"/>
-        </div>
-</div>
-     {/* -------------button---------- */}
-        <div className="">
-            <button type="submit"  className="btn_1">Register</button>
-        </div>  
+                    <div className='contactNumber'>
+                        <label className='form__label'>Contact Number: </label>
+                        <input className="form__input"
+                            type="number"
+                            name="contactNumber"
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    contactNum: e.target.value,
+                                })
+                            }
+                            placeholder="Contact Number" />
+                    </div>
+                </div>
+                {/* -------------button---------- */}
+                <div className="">
+                    <button type="submit" className="btn_1">Register</button>
+                </div>
 
-    </form></div>
+            </form></div>
     )
 }
 

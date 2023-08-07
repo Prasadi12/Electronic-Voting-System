ec0c230fee26a0acf7060b1255291e10bafad291
A.N.P Jayamanna <86679140+Prasadi12@users.noreply.github.com>
2023-08-07 04:50:47 +0000

    update LoginForm.jsx

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -11,12 +11,12 @@ function LoginForm() {
 
   const [nic, setnic] = useState('')
   //const [markdid, setmarkedid] = useState("");
-  const navigation=useNavigate()
-  
+  const navigation = useNavigate()
+
   function submit(e) {
     e.preventDefault()
     axios.post('http://localhost:5000/api/v1/login', {
-      NIC:nic
+      NIC: nic
     }).then(res => {
       if (res.data.msg === true) {
         if (res.data?.admin) {
@@ -33,34 +33,34 @@ function LoginForm() {
   }
 
   return (
-         <div className='container div1'>
-          <img src={Logo} className = "logo1" alt=""/>
+    <div className='container div1'>
+      <img src={Logo} className="logo1" alt="" />
 
-          {/*---------------headings start------------- */}
-           <h1 className="txt1 fn">Election Name</h1>
-           <p className="txt2 fn">Year</p>
-           {/*---------------headings end------------- */}
+      {/*---------------headings start------------- */}
+      <h1 className="txt1 fn">Election Name</h1>
+      <p className="txt2 fn">Year</p>
+      {/*---------------headings end------------- */}
 
 
       {/*---------------input area------------- */}
       <form onSubmit={submit}>
-           <input type="text" value={nic} onChange={
-            (e)=>setnic(e.target.value)
-            } 
-            required name="" placeholder="Enter NIC" className=" div2"/>
-           
-           
-           {/*---------------buttons start------------- */}
-           <div>
-                <button className="bn8" type="submit">
-                  <span className='fn1'>Login</span>
-                </button>
-           </div>
-           {/*---------------buttons end------------- */}
+        <input type="text" value={nic} onChange={
+          (e) => setnic(e.target.value)
+        }
+          required name="" placeholder="Enter NIC" className=" div2" />
+
+
+        {/*---------------buttons start------------- */}
+        <div>
+          <button className="bn8" type="submit">
+            <span className='fn1'>Login</span>
+          </button>
+        </div>
+        {/*---------------buttons end------------- */}
 
         <p className="pbt fn" ><center>Electronic voting system</center></p>
       </form>
-		</div>
+    </div>
   )
 }
 

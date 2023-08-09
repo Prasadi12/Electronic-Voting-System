abccb38529fc2aa5c678a0a18aef22bbe40a43d9
A.N.P Jayamanna <86679140+Prasadi12@users.noreply.github.com>
2023-08-09 10:24:28 +0000

    updated structure

--- src/pages/Admin/AdminRegistration/AdminRegistrationForm.jsx
+++ src/pages/Admin/AdminRegistration/AdminRegistrationForm.jsx
@@ -64,7 +64,12 @@ function AdminRegistrationForm(event) {
 
                     <div className='nicNumber'>
                         <label className='form__label'>NIC Number: </label>
-                        <input className='form__input' type="text" name="ssn" onChange={(e) => setFormData({ ...formData, nicNum: e.target.value, }) } placeholder="NIC Number" />
+                        <input 
+                            className='form__input' 
+                            type="text" 
+                            name="ssn" 
+                            onChange={(e) => setFormData({ ...formData, nicNum: e.target.value, }) } 
+                            placeholder="NIC Number" />
                     </div>
 
                     <div className='birthday'>
